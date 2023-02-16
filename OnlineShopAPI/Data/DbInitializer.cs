3939cfe5bb0d6de5218554567dcc363054d84757
H-Morteza <Hmorteza818@gmail.com>
2023-02-16 12:40:34 +0330

    fix and some change in DiscountPercent and Rate

--- OnlineShopAPI/Data/DbInitializer.cs
+++ OnlineShopAPI/Data/DbInitializer.cs
@@ -1,16 +1,17 @@
-using OnlineShopAPI.Entities;
 using Bogus;
+using OnlineShopAPI.Entities;
 
 namespace OnlineShopAPI.Data
 {
     public static class DbInitializer
     {
-        public static void Initialize(ShopContext context){
-            if(context.Products.Any()) return;
+        public static void Initialize(ShopContext context)
+        {
+            if (context.Products.Any()) return;
             var faker = new Faker();
             for (int i = 0; i < 20; i++)
             {
-                Product product = new Product
+                Product product = new()
                 {
                     Name = faker.Commerce.ProductName(),
                     Description = faker.Commerce.ProductDescription(),
@@ -19,16 +20,17 @@ namespace OnlineShopAPI.Data
                     Price = faker.Random.Long(0, 1000),
                     Type = faker.Commerce.ProductMaterial(),
                     QuantityInStock = faker.Random.Int(0, 500),
-                    Rate = faker.Random.Double(0, 5),
-                    PayablePrice = 0,                  
+                    Rate = Math.Round(faker.Random.Double(1, 5), 1),
+                    PayablePrice = 0,
                 };
-                #region -------------------- create payble price random------------------
+                #region -------------------- create payble price random (actuly we create discount)------------------
                 int random = faker.Random.Int(0, 1000);
-                if(random % 2 == 0 && random % 5 == 0){
-                    double discountPercent = faker.Random.Double(0, 0.5);
+                if (random % 2 == 0 && random % 5 == 0)
+                {
+                    double discountPercent = Math.Round(faker.Random.Double(0.1, 0.5), 2);
                     long finalPayblePrice = product.Price - (long)(product.Price * discountPercent);
                     product.PayablePrice = finalPayblePrice;
-                    product.DiscountPercent = (int)(discountPercent * 10);
+                    product.DiscountPercent = (int)(discountPercent * 100);
                 }
                 #endregion --------------------------------------------------------
                 context.Add(product);
