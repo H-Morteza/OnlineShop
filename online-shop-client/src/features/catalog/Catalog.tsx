ecb772cca6cadc8f24bc0d4ff4e2fdbf09d41edf
H-Morteza <Hmorteza818@gmail.com>
2023-02-18 08:47:29 +0330

    using axios instead of fetch in Catalog page

--- online-shop-client/src/features/catalog/Catalog.tsx
+++ online-shop-client/src/features/catalog/Catalog.tsx
@@ -1,14 +1,19 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+    axios
+      .get("http://localhost:5000/api/products")
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   }, []);
+
   return (
     <>
       <ProductList products={products} />
