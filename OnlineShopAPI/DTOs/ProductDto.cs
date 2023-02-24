6365c9fb864ee585a1d5d0b44409358571f4e29a
H-Morteza <Hmorteza818@gmail.com>
2023-02-24 20:12:11 +0330

    add DTOs

---
+++ OnlineShopAPI/DTOs/ProductDto.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopAPI.DTOs
+{
+    public class ProductDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        /// <summary>
+        /// We use long beacus we using from sqlite and sqlite does not recognize the decimal and it`s eseier
+        /// total pric(price without discount)
+        /// </summary>
+        public long Price { get; set; }
+        /// <summary>
+        /// Payable price(price after discount)
+        /// </summary>
+        public long PayablePrice { get; set; }
+        /// <summary>
+        /// discount percent
+        /// </summary>
+        public int DiscountPercent { get; set; }
+        /// <summary>
+        /// product image
+        /// </summary>
+        public string ImageUrl { get; set; }
+        public string ImageAlt { get; set; }
+        /// <summary>
+        /// product type
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// product brand
+        /// </summary>
+        public string Brand { get; set; }
+        /// <summary>
+        /// number of goods in stock
+        /// </summary>
+        public int QuantityInStock { get; set; }
+        /// <summary>
+        /// product rate
+        /// </summary>
+        public double Rate { get; set; }
+    }
+}
