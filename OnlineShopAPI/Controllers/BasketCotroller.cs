a139de2cb061448977900aa05b5e313300a6e4d0
H-Morteza <Hmorteza818@gmail.com>
2023-02-25 22:46:55 +0330

    some change in basket logic and basket controller

--- OnlineShopAPI/Controllers/BasketCotroller.cs
+++ OnlineShopAPI/Controllers/BasketCotroller.cs
@@ -20,59 +20,34 @@ namespace OnlineShopAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<BasketDto>> GetBasket()
         {
-            var basket = await RetrieveBasket();
+            var basketItem = new BasketLogic(_context, HttpContext);
+            var basket = await basketItem.RetrieveBasket();
             if (basket == null) return NotFound();
-
             var basketDto = _mapper.Map<BasketDto>(basket);
             return basketDto;
         }
-
-        private async Task<BasketEntity> RetrieveBasket()
-        {
-            return await _context.Baskets
-                           .Include(i => i.Items)
-                           .ThenInclude(p => p.Product)
-                           .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
-        }
-
         [HttpPost]
         public async Task<ActionResult> AddItemToBaske(long productId, int quantity)
         {
-            var basket = await RetrieveBasket();
-            if (basket == null) basket = CreatBasket();
+            var basketItem = new BasketLogic(_context, HttpContext);
+
+            var basket = await basketItem.RetrieveBasket();
+            if (basket == null) basket = basketItem.CreatBasket();
             var product = await _context.Products.FindAsync(productId);
             if (product == null) return NotFound();
-            var basketItem = new BasketLogic();
+          
             basketItem.AddItem(basket.Items, product, quantity);
             var result = await _context.SaveChangesAsync();
             if (result > 0) return StatusCode(201);
             return BadRequest(new ProblemDetails { Title = "Problem saving item to basket" });
         }
 
-        private BasketEntity CreatBasket()
-        {
-            var buyerId = Guid.NewGuid().ToString();
-            var cookieOptions = new CookieOptions
-            {
-                IsEssential = true,
-                Expires = DateTime.Now.AddDays(30)
-            };
-            Response.Cookies.Append("buyerId", buyerId, cookieOptions);
-            var basket = new BasketEntity
-            {
-                BuyerId = buyerId,
-            };
-            _context.Baskets.Add(basket);
-            return basket;
-        }
-
         [HttpDelete]
-        public async Task<ActionResult> RemoveItemFromBaske(long productId, int quantity)
+        public async Task<ActionResult> RemoveItemFromBasket(long productId, int quantity)
         {
-            var basket = await RetrieveBasket();
-
+            var basketItem = new BasketLogic(_context, HttpContext);
+            var basket = await basketItem.RetrieveBasket();
             if (basket == null) return NotFound();
-            var basketItem = new BasketLogic();
             basketItem.RemoveItem(basket.Items, productId, quantity);
             var result = await _context.SaveChangesAsync();
             if (result > 0) return StatusCode(201);
--- OnlineShopAPI/Logics/BasketLogic.cs
+++ OnlineShopAPI/Logics/BasketLogic.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineShopAPI.Data;
 using OnlineShopAPI.Entities;
 
 namespace OnlineShopAPI.Logics
 {
     public class BasketLogic
     {
+        private readonly ShopContext _context;
+        private readonly HttpContext _httpContext;
+        public BasketLogic(ShopContext context, HttpContext httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
         public void AddItem(List<BasketItemEntity> basketItems, ProductEntiy product, int quantity)
         {
             if (basketItems.All(item => item.ProductId != product.Id))
@@ -26,7 +36,31 @@ namespace OnlineShopAPI.Logics
             var item = basketItems.FirstOrDefault(item => item.ProductId == productId);
             if (item == null) return;
             item.Quantity -= quantity;
-            if (item.Quantity == 0) basketItems.Remove(item);
+            if (item.Quantity <= 0) basketItems.Remove(item);
+        }
+
+        public async Task<BasketEntity> RetrieveBasket()
+        {
+            return await _context.Baskets
+                           .Include(i => i.Items)
+                           .ThenInclude(p => p.Product)
+                           .FirstOrDefaultAsync(x => x.BuyerId == _httpContext.Request.Cookies["buyerId"]);
+        }
+        public BasketEntity CreatBasket()
+        {
+            var buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions
+            {
+                IsEssential = true,
+                Expires = DateTime.Now.AddDays(30)
+            };
+            _httpContext.Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+            var basket = new BasketEntity
+            {
+                BuyerId = buyerId,
+            };
+            _context.Baskets.Add(basket);
+            return basket;
         }
     }
 }
