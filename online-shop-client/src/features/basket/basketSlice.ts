bf14ca985c497dcbb42369b15ed5c446de9c0f5c
H-Morteza <Hmorteza818@gmail.com>
2023-04-12 23:51:39 +0430

    using Thunk for async metod for add item

--- online-shop-client/src/features/basket/basketSlice.ts
+++ online-shop-client/src/features/basket/basketSlice.ts
@@ -1,14 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { Basket } from "../../app/models/basket";
+import apiHelper from "../../app/api/apiHelper";
 
 interface BasketState {
   basket: Basket | null;
+  status: string;
 }
 
 const initialState: BasketState = {
   basket: null,
+  status: "complete",
 };
 
+export const addBasketItemAsync = createAsyncThunk<
+  Basket,
+  { productId: number; quntity: number }
+>("", async ({ productId, quntity }) => {
+  try {
+    return await apiHelper.Basket.addItem(productId, quntity);
+  } catch (error) {
+    console.log(error);
+  }
+});
 export const basketSlice = createSlice({
   name: "basket",
   initialState,
@@ -26,6 +39,18 @@ export const basketSlice = createSlice({
       if (state.basket?.items[itemIndex].quantity === 0)
         state.basket.items.splice(itemIndex, 1);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addBasketItemAsync.pending, (state, action) => {
+      state.status = "pendingAddItem" + action.meta.arg.productId;
+    });
+    builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
+      state.basket = action.payload;
+      state.status = "complete";
+    });
+    builder.addCase(addBasketItemAsync.pending, (state) => {
+      state.status = "complete";
+    });
   },
 });
 export const { setBasket, removeItem } = basketSlice.actions;
