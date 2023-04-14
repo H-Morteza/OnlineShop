08d4abed8b7cdcb71264fe5d3e30641e61b3f271
H-Morteza <Hmorteza818@gmail.com>
2023-04-14 14:51:52 +0430

    using thunk in basketProductListComponent

--- online-shop-client/src/features/basket/basketSlice.ts
+++ online-shop-client/src/features/basket/basketSlice.ts
@@ -22,22 +22,24 @@ export const addBasketItemAsync = createAsyncThunk<
     console.log(error);
   }
 });
+
+export const removeBasketItemAsync = createAsyncThunk<
+  void,
+  { productId: number; quntity: number; name?: string }
+>("basket/removeBasketItemAsync", async ({ productId, quntity }) => {
+  try {
+    await apiHelper.Basket.removeItem(productId, quntity);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 export const basketSlice = createSlice({
   name: "basket",
   initialState,
   reducers: {
     setBasket: (state, action) => {
       state.basket = action.payload;
-    },
-    removeItem: (state, action) => {
-      const { productId, quantity } = action.payload;
-      const itemIndex = state.basket?.items.findIndex(
-        (i) => i.productId === productId
-      );
-      if (itemIndex === -1 || itemIndex === undefined) return;
-      state.basket!.items[itemIndex].quantity -= quantity;
-      if (state.basket?.items[itemIndex].quantity === 0)
-        state.basket.items.splice(itemIndex, 1);
     },
   },
   extraReducers: (builder) => {
@@ -51,6 +53,25 @@ export const basketSlice = createSlice({
     builder.addCase(addBasketItemAsync.rejected, (state) => {
       state.status = "complete";
     });
+
+    builder.addCase(removeBasketItemAsync.pending, (state, action) => {
+      state.status =
+        "pendingRemoveItem" + action.meta.arg.productId + action.meta.arg.name;
+    });
+    builder.addCase(removeBasketItemAsync.fulfilled, (state, action) => {
+      const { productId, quntity } = action.meta.arg;
+      const itemIndex = state.basket?.items.findIndex(
+        (i) => i.productId === productId
+      );
+      if (itemIndex === -1 || itemIndex === undefined) return;
+      state.basket!.items[itemIndex].quantity -= quntity;
+      if (state.basket?.items[itemIndex].quantity === 0)
+        state.basket.items.splice(itemIndex, 1);
+      state.status = "complete";
+    });
+    builder.addCase(removeBasketItemAsync.rejected, (state) => {
+      state.status = "complete";
+    });
   },
 });
-export const { setBasket, removeItem } = basketSlice.actions;
+export const { setBasket } = basketSlice.actions;
