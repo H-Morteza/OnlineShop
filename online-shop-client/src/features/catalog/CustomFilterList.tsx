4766dda59fb832a59e53b3139ab27cb53a46dab8
H-Morteza <Hmorteza818@gmail.com>
2023-05-01 09:15:17 +0430

    improve ProductListLogic CustomFilterList component

--- online-shop-client/src/features/catalog/CustomFilterList.tsx
+++ online-shop-client/src/features/catalog/CustomFilterList.tsx
@@ -5,37 +5,34 @@ import {
   TextField,
   Checkbox,
 } from "@mui/material";
-import CustomSlider from "../catalog/CustomSlider";
 interface Props {
   customLists: string[];
 }
 export default function CustomFilterList({ customLists }: Props) {
   return (
     <Box sx={{ padding: "15px 15px 0 15px" }}>
-      <Box sx={{ padding: "15px 15px 0 15px" }}>
-        <TextField
-          sx={{ paddingBottom: 2 }}
-          id="outlined-search"
-          label="Search types"
-          type="search"
-          fullWidth
-        />
-        <FormGroup
-          sx={{
-            maxBlockSize: 150,
-            overflowY: "auto",
-            display: "grid",
-          }}
-        >
-          {customLists.map((customList) => (
-            <FormControlLabel
-              control={<Checkbox />}
-              label={customList}
-              key={customList}
-            />
-          ))}
-        </FormGroup>
-      </Box>
+      <TextField
+        sx={{ paddingBottom: 2 }}
+        id="outlined-search"
+        label="Search types"
+        type="search"
+        fullWidth
+      />
+      <FormGroup
+        sx={{
+          maxBlockSize: 150,
+          overflowY: "auto",
+          display: "grid",
+        }}
+      >
+        {customLists.map((customList) => (
+          <FormControlLabel
+            control={<Checkbox />}
+            label={customList}
+            key={customList}
+          />
+        ))}
+      </FormGroup>
     </Box>
   );
 }
