950a91ad89c4968ca127d0067e2f11ea5e1cba2c
H-Morteza <Hmorteza818@gmail.com>
2023-05-15 08:33:26 +0330

    completed CustomFilterList component

--- online-shop-client/src/features/catalog/CustomFilterList.tsx
+++ online-shop-client/src/features/catalog/CustomFilterList.tsx
@@ -5,10 +5,40 @@ import {
   TextField,
   Checkbox,
 } from "@mui/material";
+import { useState } from "react";
 interface Props {
   customLists: string[];
+  checked?: string[];
+  onChange: (Items: string[]) => void;
 }
-export default function CustomFilterList({ customLists }: Props) {
+
+export default function CustomFilterList({
+  customLists,
+  checked,
+  onChange,
+}: Props) {
+  const [searchList, setSearchList] = useState(customLists);
+  const [checkedItems, setCheckedItems] = useState(checked || []);
+
+  function handelCheced(value: string) {
+    const currentIndex = checkedItems.findIndex((item) => item === value);
+    let newChecked: string[];
+    if (currentIndex === -1) newChecked = [...checkedItems, value];
+    else newChecked = checkedItems.filter((item) => item !== value);
+    setCheckedItems(newChecked);
+    onChange(newChecked);
+  }
+  function search(e: any) {
+    let searchValue = e.target.value;
+    let newList = customLists.filter((list) =>
+      list.toLowerCase().includes(searchValue)
+    );
+    setSearchList(newList);
+  }
+  function check(item: string): boolean {
+    let t = checkedItems.indexOf(item) !== -1;
+    return t;
+  }
   return (
     <Box sx={{ padding: "15px 15px 0 15px" }}>
       <TextField
@@ -16,6 +46,9 @@ export default function CustomFilterList({ customLists }: Props) {
         id="outlined-search"
         label="Search types"
         type="search"
+        onChange={(e) => {
+          search(e);
+        }}
         fullWidth
       />
       <FormGroup
@@ -25,11 +58,16 @@ export default function CustomFilterList({ customLists }: Props) {
           display: "grid",
         }}
       >
-        {customLists.map((customList) => (
+        {searchList.map((item) => (
           <FormControlLabel
-            control={<Checkbox />}
-            label={customList}
-            key={customList}
+            control={
+              <Checkbox
+                onClick={() => handelCheced(item)}
+                checked={check(item)}
+              />
+            }
+            label={item}
+            key={item}
           />
         ))}
       </FormGroup>
