7c69b71fede2e8663d25338c7cbfe219809b60b7
H-Morteza <Hmorteza818@gmail.com>
2023-05-30 07:43:06 +0330

    add AccountController to login and register

---
+++ OnlineShopAPI/DTOs/Request/LoginReuquestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShopAPI.DTOs.Request
+{
+    public class LoginReuquestDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
