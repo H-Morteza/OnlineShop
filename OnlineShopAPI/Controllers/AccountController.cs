6326aa44aeb0d9fe1a3b235606bd4c544005a229
H-Morteza <Hmorteza818@gmail.com>
2023-06-01 08:53:03 +0330

    Validating the token on the server and configuring swagger to send the auth token

--- OnlineShopAPI/Controllers/AccountController.cs
+++ OnlineShopAPI/Controllers/AccountController.cs
@@ -1,25 +1,35 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShopAPI.DTOs.Request;
+using OnlineShopAPI.DTOs.Response;
 using OnlineShopAPI.Entities;
+using OnlineShopAPI.Services;
 
 namespace OnlineShopAPI.Controllers
 {
     public class AccountController : BaseApiController
     {
         private readonly UserManager<User> _userManager;
-        public AccountController(UserManager<User> userManager)
+        private readonly TokenService _tokenService;
+
+        public AccountController(UserManager<User> userManager, TokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<User>> Login(LoginReuquestDto loginReuquest)
+        public async Task<ActionResult<UserResponseDto>> Login(LoginReuquestDto loginReuquest)
         {
             var user = await _userManager.FindByNameAsync(loginReuquest.UserName);
             if (user == null || !await _userManager.CheckPasswordAsync(user, loginReuquest.Password))
                 return Unauthorized();
-            return user;
+            return new UserResponseDto()
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateToken(user),
+            };
         }
         [HttpPost("register")]
         public async Task<ActionResult> Register(RegisterReuquestDto registerReuquest)
@@ -41,6 +51,20 @@ namespace OnlineShopAPI.Controllers
             }
             await _userManager.AddToRoleAsync(user, "Member");
             return StatusCode(201);
+        }
+
+        [Authorize]
+        [HttpGet("currentUser")]
+        public async Task<ActionResult<UserResponseDto>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            //if (user == null || !await _userManager.CheckPasswordAsync(user, loginReuquest.Password))
+            //    return Unauthorized();
+            return new UserResponseDto()
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateToken(user),
+            };
         }
     }
 }
--- OnlineShopAPI/Program.cs
+++ OnlineShopAPI/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OnlineShopAPI.CustomMiddleware;
 using OnlineShopAPI.Data;
 using OnlineShopAPI.Entities;
+using OnlineShopAPI.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +16,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Jwt aut header",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference =new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "aouth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddDbContext<ShopContext>(options =>
 {
@@ -19,11 +50,27 @@ builder.Services.AddDbContext<ShopContext>(options =>
 });
 builder.Services.AddCors();
 
-builder.Services.AddIdentityCore<User>()
+builder.Services.AddIdentityCore<User>(option =>
+{
+    option.User.RequireUniqueEmail = true;
+})
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ShopContext>();
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
+        };
+    });
+
 builder.Services.AddAuthorization();
+builder.Services.AddScoped<TokenService>();
 
 var app = builder.Build();
 
@@ -36,8 +83,9 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.UseCors(opt =>
 {
     opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
